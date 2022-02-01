415a8c144e82b15c3e0b66ada9e1f74d390f852b
Marsel <you@example.com>
2022-02-01 10:36:40 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Categories from './component/Categories'
 import Header from './component/Header'
 import Menu from './component/Menu'
 
 function App() {
+  const [menuItems, SetMenuItems] = useState([])
+  const [categories, SetCategories] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/menu`)
+      .then(resp => resp.json())
+      .then(menuFromServer => SetMenuItems(menuFromServer))
+  }, [])
+
+
  
   return  <main>
   <section className="menu section">
@@ -11,7 +22,7 @@ function App() {
 
     <Categories />
 
-    <Menu />    
+    <Menu menuItems={menuItems} />    
   </section>
 </main>
    
--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,35 +1,17 @@
-function Menu(){
+function Menu({ menuItems }){
 return <div className='section-center'>
-<article className='menu-item'>
-  <img src="src/images/item-1.jpeg" alt="" className='photo' />
+{menuItems.map((Item) => {
+return <article className='menu-item'>
+  <img src={Item.img} alt={Item.tile} className='photo' />
   <div className='item-info'>
     <header>
-      <h4>Title</h4>
-      <h4 className='price'>$22</h4>
+      <h4>{Item.title}</h4>
+      <h4 className='price'>${Item.price}</h4>
     </header>
-    <p className='item-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt minus nam magnam, iusto qui sunt possimus quas nemo itaque id eligendi tenetur doloremque neque natus nobis provident. Consequuntur, quo. Veritatis.</p>
+    <p className='item-text'>{Item.desc}</p>
   </div>
 </article>
-<article className='menu-item'>
-  <img src="src/images/item-1.jpeg" alt="" className='photo' />
-  <div className='item-info'>
-    <header>
-      <h4>Title</h4>
-      <h4 className='price'>$22</h4>
-    </header>
-    <p className='item-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt minus nam magnam, iusto qui sunt possimus quas nemo itaque id eligendi tenetur doloremque neque natus nobis provident. Consequuntur, quo. Veritatis.</p>
-  </div>
-</article>
-<article className='menu-item'>
-  <img src="src/images/item-1.jpeg" alt="" className='photo' />
-  <div className='item-info'>
-    <header>
-      <h4>Title</h4>
-      <h4 className='price'>$22</h4>
-    </header>
-    <p className='item-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt minus nam magnam, iusto qui sunt possimus quas nemo itaque id eligendi tenetur doloremque neque natus nobis provident. Consequuntur, quo. Veritatis.</p>
-  </div>
-</article>
+})}
 </div>
 }
 export default Menu
